11df87511b9a1de154bfd7d006cd67a8ce29adef
unknown <affanashfaq786@gmail.com>
2022-11-02 14:39:36 -0700

    Complete Project

---
+++ assignment.js
@@ -0,0 +1,50 @@
+var Urdu = +prompt("Urdu", "Enter Your Marks")
+var Mathamatics = +prompt("Mathamatice", "Enter Your Marks")
+var Islamiat = +prompt("Islamiat", "Enter Your Marks")
+var Geography = +prompt("Geography", "Enter Your Marks")
+var TotalMarks = Urdu+Mathamatics+Islamiat+Geography;
+var Percentage = (TotalMarks/500)*100
+
+
+var Fruit1 = "Mango"
+var Fruit2 = "Orange"
+var Fruit3 = "Grapes"
+var Fruit4 = "Pineapple"
+var Fruit5 = "Banana"
+
+var Fruit = ["Mango","Orange","Grapes","Pineapple","Banana"]
+console.log("Fruits")[2]
+
+var fruits = "Mango"
+var fruits = "Apple"
+var fruits = "Grapes"
+var fruits = "Banana"
+var fruits = "Orange"
+
+var fruits = ["mango","apple","grapes","banana","orange",];
+console.log(02);
+
+
+
+var officer = true
+var Assisstent = true
+if(officer == true){console.log('Document Submitted To offficer')}
+else if(Assisstent == false ){console.log('Document Submitted To Assisstent')}
+else{console.log('Return')}
+var officer = true
+if(officer == true){console.log('Document Submitted')}
+else{console.log('Return')}
+
+
+
+var num = prompt('Marks')
+if(num >=50 && num <=60)
+{console.log('c')}
+else if(num >=60 && num <=70)
+{console.log('b')}
+else if(num >=70 && num <=80)
+{console.log('A')}
+else if(num >=80 && num <=90)
+{console.log('A+')}
+else if(num >=90 && num <=100)
+{console.log('A+1')}
